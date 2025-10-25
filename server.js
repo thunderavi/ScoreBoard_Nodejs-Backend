810c60fe5364fcc93e1bbcb3b8d8b93699e7e461
thunderavi <hittuprasad0909@gmail.com>
2025-10-25 16:06:29 +0530

    updated cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 // CORS Configuration - MUST BE FIRST
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://score-board-nodejs-backend.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
